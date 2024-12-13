186f762700ffd4d90151a26954a6155a0e0d54aa
AliBinary <AliGhanbariCs@gmail.com>
2024-12-13 12:25:46 +0330

    last chance

--- Ali/I.cpp
+++ Ali/I.cpp
@@ -2,20 +2,85 @@
 using namespace std;
 
 #define ll long long
-const int N = 1e5 + 5;
+
+void find_max_val(int &n, vector<vector<ll>> &val, vector<vector<bool>> &safe, int &mx, int &my, ll &mval)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (!safe[i][j])
+                if (val[i][j] > mval)
+                {
+                    mval = val[i][j];
+                    mx = i;
+                    my = j;
+                }
+        }
+    }
+}
+void del_mval(ll &ans, int &dis, vector<vector<int>> &mat, int &n, vector<vector<ll>> &val, vector<vector<bool>> &safe, int &mx, int &my, ll &mval)
+{
+    ans = mat[mx][my];
+    for (int x = max(0, mx - dis); x <= min(n - 1, mx + dis); x++)
+    {
+        for (int y = max(0, my - dis); y <= min(n - 1, my + dis); y++)
+        {
+            safe[x][y] = 1;
+            val[x][y] = 0;
+        }
+    }
+}
 
 void Main()
 {
     int n;
     cin >> n;
-    vector<vector<int>> matrix(n, vector<int>(n, 0));
-    for (auto &row : matrix)
+
+    vector<vector<int>> mat(n, vector<int>(n, 0));
+    for (auto &row : mat)
     {
         for (auto &x : row)
         {
             cin >> x;
         }
     }
+
+    int dis = floor(n / 2.);
+    vector<vector<bool>> safe(n, vector<bool>(n, 0));
+
+    vector<vector<ll>> val(n, vector<ll>(n, 0));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int x = max(0, i - dis); x <= min(n - 1, i + dis); x++)
+            {
+                for (int y = max(0, j - dis); y <= min(n - 1, j + dis); y++)
+                {
+                    val[i][j] += mat[x][y];
+                }
+            }
+            val[i][j] -= mat[i][j];
+        }
+    }
+
+    // for (auto &row : val)
+    // {
+    //     for (auto &x : row)
+    //     {
+    //         cout << x << ' ';
+    //     }
+    //     cout << endl;
+    // }
+
+    ll ans = 0;
+    int mx = 0, my = 0;
+    ll mval = -1;
+    find_max_val(n, val, safe, mx, my, mval);
+
+    del_mval(ans, dis, mat, n, val, safe, mx, my, mval);
 }
 
 int main()
